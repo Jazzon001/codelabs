866476db9f5f7749bc281a0f3c9a02f6dd63b2a3
Jazzon001 <jasson.16jj@gmail.com>
2024-09-08 22:36:46 -0600

    Subiendo tarea completa FireBase

--- firebase-get-to-know-flutter/step_02/lib/home_page.dart
+++ firebase-get-to-know-flutter/step_02/lib/home_page.dart
@@ -2,9 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart' // new
+    hide EmailAuthProvider, PhoneAuthProvider;    // new
+import 'package:flutter/material.dart';           // new
+import 'package:provider/provider.dart';          // new
 
+import 'app_state.dart';                          // new
+import 'src/authentication.dart';                 // new
 import 'src/widgets.dart';
+import 'guest_book.dart';  
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -21,6 +27,15 @@ class HomePage extends StatelessWidget {
           const SizedBox(height: 8),
           const IconAndDetail(Icons.calendar_today, 'October 30'),
           const IconAndDetail(Icons.location_city, 'San Francisco'),
+          // Add from here
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => AuthFunc(
+                loggedIn: appState.loggedIn,
+                signOut: () {
+                  FirebaseAuth.instance.signOut();
+                }),
+          ),
+          // to here
           const Divider(
             height: 8,
             thickness: 1,
@@ -32,6 +47,32 @@ class HomePage extends StatelessWidget {
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
           ),
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                switch (appState.attendees) {
+        1 => const Paragraph('1 person going'),
+        >= 2 => Paragraph('${appState.attendees} people going'),
+        _ => const Paragraph('No one going'),
+      },
+                if (appState.loggedIn) ...[
+                  YesNoSelection(
+          state: appState.attending,
+          onSelection: (attending) => appState.attending = attending,
+        ),
+                  const Header('Discussion'),
+                  GuestBook(
+                    addMessage: (message) =>
+                        appState.addMessageToGuestBook(message),
+                        messages: appState.guestBookMessages,
+                  ),
+                ],
+              ],
+            ),
+          ),
+          const Header('Discussion'),
+          GuestBook(addMessage: (message) => print(message)),
         ],
       ),
     );
